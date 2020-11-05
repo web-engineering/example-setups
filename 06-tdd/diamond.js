ea96046a4afe601bd2b49793c04b351aa3f1664e
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2020-11-05 14:12:47 +0100

    starting point for fizzbuzz and test

--- 06-tdd/diamond.js
+++ 06-tdd/diamond.js
@@ -1,38 +1,7 @@
-function listdoubler(l){
-  let len = l.length;
-  for(let z = len-2; z>= 0; z--) {
-    l.push( l[z] );
-  }
-  return l;
-}
 
-function stringdoubler(s){
-  let len = s.length;
-  for(let z = len-2; z>= 0; z--) {
-    s += s[z];
-  }
-  return s;
-}
-
-function stringcreator(char, pos, max) {
-  let whitespace = "............................................................";
-
-  let post =  whitespace.substr(0, max - pos -1);
-  let pre = whitespace.substr(0, pos);
-  return pre + char + post;  
-}
 
 function diamond(letter) {
-  const max = letter.charCodeAt(0) - 'A'.charCodeAt(0);
-  let result = [];
-  let l,s;
-  for(let pos = 0;  pos <= max; pos++ ) {
-    l = String.fromCharCode(65 + pos);
-    s = stringcreator(l, max-pos, max+1);
-    result.push(stringdoubler(s));
-  }
-
-  return listdoubler(result);
+  return ['A'];
 }
 
-export { diamond, listdoubler, stringdoubler, stringcreator };
+export { diamond };
--- 06-tdd/fizzbuzz.js
+++ 06-tdd/fizzbuzz.js
@@ -1,31 +1,6 @@
-function fuzzer(i) {
-  let s = `${i}`;
-
-  if (i % 15 === 0) {
-    return "FizzBuzz";
-  } else if ( s.includes('3') && s.includes('5') ) {
-    return "FizzBuzz";    
-  } else if ( s.includes('5') && i % 3 === 0 ) {
-    return "FizzBuzz";       
-  } else if (i % 5 === 0) {
-    return 'Buzz';
-  } else if (i % 3 === 0) {
-    return 'Fizz';
-  } else if ( s.includes('3')) {
-    return "Fizz";
-  } else if ( s.includes('5')) {
-    return "Buzz";
-  }  else {
-    return i;
-  }    
-}
-
 function fizzbuzz(n){
-  const result = [];
-  for (let i = 1; i <= n; i++) {
-    result.push(fuzzer(i));
-  }
+  const result = [1];
   return result;
 }
 
-export { fizzbuzz, fuzzer }
+export { fizzbuzz }
